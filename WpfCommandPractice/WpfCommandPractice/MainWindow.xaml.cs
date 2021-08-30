77375e24129bd2d9cda8fe57743af215165755a9
むずでょ <muzudho1@gmail.com>
2021-08-30 23:40:26 +0900

    Command

---
+++ WpfCommandPractice/WpfCommandPractice/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace WpfCommandPractice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// ユーザーのアクションと、その処理を結びつけるもの
+        /// </summary>
+        public static RoutedCommand AlertCommand = new RoutedCommand();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Hello world");
+        }
+
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = this.checkBox.IsChecked.Value;
+        }
+    }
+}
